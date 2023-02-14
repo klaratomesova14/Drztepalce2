0e427a10bd4fb925954675053c00d823a1128175
Klara Tomesova <klaratomesova14@gmail.com>
2023-02-14 09:30:56 +0100

    new drztepalce github

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+module.exports = function(eleventyConfig) {
+
+    // Výchozí výstupní složka: _site
+  
+    // Zkopírovat images/ do _site/images
+    eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("favicon");
+  
+    // Zkopírovat css/ to _site/css/
+    eleventyConfig.addPassthroughCopy("css");
+  
+    return {
+      // možné formáty šablon
+      templateFormats: ["njk", "html", "md", "liquid"],
+  
+      // jako šablonovací jazyk zvolíme Nunjucks
+      markdownTemplateEngine: "njk",
+      htmlTemplateEngine: "njk",
+      dataTemplateEngine: "njk",
+    
+    }
+  
+  };
